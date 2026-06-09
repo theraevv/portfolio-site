79920ffbb2dfb2354877c669c74bd769f5218d5a
Raev Epong <raev.epong@outlook.com>
2026-06-09 19:42:46 +0800

    Define window.API_BASE in config.js to avoid 'API_BASE is not defined' errors

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-const API_BASE = "";
+// API base URL for backend API. Leave empty string to use same-origin.
+// You can set `window.API_BASE` before this script loads to override.
+window.API_BASE = window.API_BASE || "";
